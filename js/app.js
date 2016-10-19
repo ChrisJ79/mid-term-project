3f5263049f069ed98e958874562ac25fecd96094
Chris Johnson <csjohnson79@icloud.com>
2016-10-19 13:07:55 -0600

    Completed top part of order form, got angular to return input on lower half of page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,50 @@
 
+// VERSION 1
+
+angular.module("MyApp", [])
+    .controller("MainCtrl", mainCtrl);
+
+
+    function mainCtrl() {
+    	var timber = this;
+    	timber.sizes = [""];
+    	timber.qty = [""];
+    	timber.addTimber = function() {
+			timber.sizes.push(timber.newTimber);
+			timber.qty.push(timber.newNum);
+			console.log(timber.sizes);
+		};	
+
+
+		// timber.addQty = function()	 {
+		// 	timber.qty.push(timber.newNum);
+  //    		console.log(timber.qty);
+  //    	};	
+}
+
+
+
+
+// VERSION 2
+
+// var app = angular.module("MyApp", []);
+
+//     app.controller("MainCtrl", function (timber) {
+//     	var timber = this;
+//     	timber.sizes = [""];
+//     	// timber.qty = "";
+//     	timber.addTimber = function () {
+// 			timber.sizes.push(timber.newTimber);
+// 			// timber.qty.push(timber.newNum)
+//         };
+// });
+
+
+
+//     var app = angular.module("myShoppingList", []);
+// app.controller("myCtrl", function($scope) {
+//     $scope.products = ["Milk", "Bread", "Cheese"];
+//     $scope.addItem = function () {
+//         $scope.products.push($scope.addMe);
+//     }
+// });
