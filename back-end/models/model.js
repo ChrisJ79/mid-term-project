56393b6e365ad1d70ad5126c0b72d8e3a20a5cb9
Chris Johnson <csjohnson79@icloud.com>
2016-11-07 20:37:36 -0700

    created schema in model.js

--- back-end/models/model.js
+++ back-end/models/model.js
@@ -1,2 +1,18 @@
 
 //model
+
+
+var mongoose = require('mongoose');
+
+var quoteSchema = mongoose.Schema({
+    name : {type : String, required : true},
+    jobName : {},
+    contactName : {},
+    contactPhoneNumber : {},
+    emailAddress : {type : String, default : 'me@something.com'},
+    notes : {},
+
+});
+
+module.exports = mongoose.model('Hero', heroSchema, 'heroes');
+// db.heroes
