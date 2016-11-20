e2f08514b849a2f7460d5c00c4afd54997e69b77
Chris Johnson <csjohnson79@icloud.com>
2016-11-19 18:46:05 -0700

    created server using Express in index.js

--- back-end/index.js
+++ back-end/index.js
@@ -3,42 +3,57 @@
 
 'use strict';
 
-//Import variables from node modules with 'requite; method'
-var express = require('express'),
-    // morgan = require('morgan'),
-    // bodyParser = require('body-parser'),
-    // cookieParser = require('cookie-parser'),
-    // Routes = require('./routes'),
-    PORT = process.env.PORT || 3000;
+// 1) Import variables from node modules with 'require' method
+var express = require('express');
+var path = require('path');
+    // var morgan = require('morgan');
+    // bodyParser = require('body-parser');
+    // cookieParser = require('cookie-parser');
+    // Routes = require('./routes');
+    // PORT = process.env.PORT || 3000;*******
+    // app.get('/', function(request, response) {
+    //     response.send("I'm working!");
+    // });
 
-// Create an 'instance' that will be used to set up Middleware
+
+
+
+// 2) Create an 'instance' that will be used to set up Middleware
 var app = express();
 
+// 3) Define the port to run on
+app.set('port', 3000);
+
+// 5) Configure Express to serve the static files the 'Public' directory, which inclues index.html.
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 4) Configure the app to listen on Port 3000
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('You gettin served on ' + port);
+});
+// app.listen(PORT, () => {
+//     console.log('Server is running on PORT:', PORT);
+// });
+// or...
+// app.listen(3000);
 
 
+// MIDDLEWARE
 
-
-//routes
-// How do I know whether to include my routes on server.js or routes.js??
-// Routes(app);
-
-
-// mdiddleware
-// app.use(express.static('public'));
 // app.use(morgan('dev'));
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 // app.use(cookieParser.urlencoded({ extended: true }));
 // app.use(cookieParser.json());
-//
+
 // app.get('/', function(req, res) {
 //     res.sendFile("index.html", { root: './mid-term-project/public' });
 // });
 
-// configure the app to listen on Port 3000
-app.listen(PORT, () => {
-    console.log('Server is running on PORT:', PORT);
-});
+//Routes
+// How do I know whether to include my routes on server.js or routes.js??
+// Routes(app);
 
 
 // Is this all I need to get a response??
